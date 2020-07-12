1f4e2ac34d3319703e2f78c9c97c7d621188c84d
acecondor <maurizio@defcon.it>
2020-07-12 13:15:11 +0200

    Aggiungere i file di progetto.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Le informazioni generali relative a un assembly sono controllate dal seguente 
+' set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+' associate a un assembly.
+
+' Controllare i valori degli attributi degli assembly
+
+<Assembly: AssemblyTitle("EdgeChromiumUninstall")>
+<Assembly: AssemblyDescription("Command line Microsoft Edge Chromium Uninstall")>
+<Assembly: AssemblyCompany("Defcon.it")>
+<Assembly: AssemblyProduct("EdgeChromiumUninstall")>
+<Assembly: AssemblyCopyright("Copyright © 2020 Maurizio Condini")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Se il progetto viene esposto a COM, il GUID seguente verrà usato come ID del typelib
+<Assembly: Guid("7df17a6d-81c4-464f-81c8-47c5de4c9ac6")>
+
+' Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+'
+'      Versione principale
+'      Versione secondaria
+'      Numero di build
+'      Revisione
+'
+' È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
+' usando l'asterisco '*' come illustrato di seguito:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("0.1.0.0")>
+<Assembly: AssemblyFileVersion("0.1.0.0")>
---
+++ modMain.vb
@@ -0,0 +1,75 @@
+﻿Imports System
+Imports System.IO
+
+Module modMain
+
+    Dim _EdgePath As String = My.Computer.FileSystem.SpecialDirectories.ProgramFiles & "\Microsoft\Edge\Application\"
+
+
+    Sub Main()
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.WriteLine("EdgeChromiumUninstall v." & My.Application.Info.Version.ToString)
+        Console.WriteLine(My.Application.Info.Copyright) 'copyright
+        Console.WriteLine()
+        Console.ForegroundColor = ConsoleColor.Yellow
+        Console.WriteLine("Il programma è un software libero; potete redistribuirlo e/o secondo i termini della come pubblicato ")
+        Console.WriteLine("dalla Free Software Foundation; sia la versione 3, sia (a vostra scelta) ogni versione successiva.")
+        Console.WriteLine()
+        Console.WriteLine("Questo programma è distribuito nella speranza che sia utile ma SENZA ALCUNA GARANZIA senza")
+        Console.WriteLine("anche l'implicita garanzia di POTER ESSERE VENDUTO o di IDONEITA' A UN PROPOSITO PARTICOLARE.")
+        Console.WriteLine("Vedere la GNU General Public License per ulteriori dettagli.")
+        Console.WriteLine("https://www.gnu.org/licenses/gpl-3.0.txt")
+        Console.WriteLine()
+        Console.ForegroundColor = ConsoleColor.White
+
+        Try
+            If Directory.Exists(_EdgePath) Then 'verifico esista cartella
+
+                For Each Dir As String In Directory.GetDirectories(_EdgePath, "*.*") 'recupero elenco cartelle 
+
+                    Dim _version As String = New DirectoryInfo(Dir).Name 'recupero nome ultima cartella percorso
+
+                    If ReturnOnlyNumeric(_version) <> Nothing Then 'elaboro solo se nome cartella numerico
+
+                        Console.WriteLine("Disinstallare la versione " & _version & "?")
+                        Console.WriteLine("Premi S per disinstallare, altro tasto per uscire")
+
+                        If Console.ReadKey.KeyChar.ToString.ToUpper = "S" Then
+                            Console.WriteLine()
+                            Process.Start(_EdgePath & _version & "\installer\setup.exe", "--uninstall --system-level --verbose-logging --force-uninstall")
+                        End If
+                    End If
+                Next
+            Else
+
+                Console.ForegroundColor = ConsoleColor.Red
+                Console.WriteLine("Non esiste la cartella " & _EdgePath)
+                Console.ForegroundColor = ConsoleColor.White
+
+            End If
+
+        Catch ex As Exception
+            Console.ForegroundColor = ConsoleColor.Red
+            Console.WriteLine(Err.Description)
+            Console.ForegroundColor = ConsoleColor.White
+        End Try
+        Console.WriteLine("Premi un tasto per uscire")
+        Console.ReadKey()
+    End Sub
+
+    Private Function ReturnOnlyNumeric(ByVal _text As String)
+        Try
+            If IsNumeric(_text) Then
+                Return _text
+            Else
+                Return Nothing
+            End If
+        Catch ex As Exception
+            Console.ForegroundColor = ConsoleColor.Red
+            Console.WriteLine(Err.Description)
+            Console.ForegroundColor = ConsoleColor.White
+            Return Nothing
+        End Try
+    End Function
+
+End Module
